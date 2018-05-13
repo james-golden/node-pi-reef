70e6e69bb8230a360e313b4935ed71ea57027d47
james-golden <james-golden@users.noreply.github.com>
2018-05-13 17:36:05 -0500

    new branch to add equipment
    added all 8 equipments that are needed for relay boards

--- misc.js
+++ misc.js
@@ -6,8 +6,68 @@ var equipment1opt = {
     }
 };
 
+var equipment2opt = {
+    url: 'http://127.0.0.1:80/api/equipments/2',
+    auth: {
+        'user': 'reef-pi',
+        'pass': 'reef-pi'
+    }
+};
 
+var equipment3opt = {
+    url: 'http://127.0.0.1:80/api/equipments/3',
+    auth: {
+        'user': 'reef-pi',
+        'pass': 'reef-pi'
+    }
+};
+
+var equipment4opt = {
+    url: 'http://127.0.0.1:80/api/equipments/4',
+    auth: {
+        'user': 'reef-pi',
+        'pass': 'reef-pi'
+    }
+};
+
+var equipment5opt = {
+    url: 'http://127.0.0.1:80/api/equipments/5',
+    auth: {
+        'user': 'reef-pi',
+        'pass': 'reef-pi'
+    }
+};
+
+var equipment6opt = {
+    url: 'http://127.0.0.1:80/api/equipments/6',
+    auth: {
+        'user': 'reef-pi',
+        'pass': 'reef-pi'
+    }
+};
+
+var equipment7opt = {
+    url: 'http://127.0.0.1:80/api/equipments/7',
+    auth: {
+        'user': 'reef-pi',
+        'pass': 'reef-pi'
+    }
+};
+
+var equipment8opt = {
+    url: 'http://127.0.0.1:80/api/equipments/8',
+    auth: {
+        'user': 'reef-pi',
+        'pass': 'reef-pi'
+    }
+};
 
 
 module.exports.equipment1opt = equipment1opt;
-
+module.exports.equipment2opt = equipment2opt;
+module.exports.equipment3opt = equipment3opt;
+module.exports.equipment4opt = equipment4opt;
+module.exports.equipment5opt = equipment5opt;
+module.exports.equipment6opt = equipment6opt;
+module.exports.equipment7opt = equipment7opt;
+module.exports.equipment8opt = equipment8opt;
